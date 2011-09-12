bcee936afe281a4e64dd60e1e7346f3268ba5383
b.j.drew <b.j.drew@2dfc3d67-8f33-ca59-f73a-e4d4cf50c7a3>
2011-09-12 01:21:02 +0000

    Merging changes from b1

--- src/emu/os/Kernel.java
+++ src/emu/os/Kernel.java
@@ -5,8 +5,11 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import emu.hw.CPU;
 import emu.hw.MMU;
@@ -20,19 +23,11 @@ public class Kernel {
 	/**
 	 * For tracing
 	 */
-	static Logger trace = Logger.getLogger("emu.os");
+	static Logger trace = Logger.getLogger("emuos");
 	/**
 	 * CPU instance
 	 */
 	CPU cpu;
-	public CPU getCpu() {
-		return cpu;
-	}
-
-	public MMU getMmu() {
-		return mmu;
-	}
-
 	/**
 	 * MMU instance
 	 */
@@ -51,26 +46,43 @@ public class Kernel {
 	BufferedWriter wr;
 	
 	/**
-	 * 
+	 * Starts EmuOS
 	 * @param args
 	 */
 	public static final void main(String[] args) {
 		
+		if (args.length != 2) {
+			trace.severe("I/O files missing.");
+			System.exit(1);
+		}
+		
 		String inputFile = args[0];
 		String outputFile = args[1];
+		
+		try {
+		    // Create an appending file handler
+		    FileHandler handler = new FileHandler("emuos.log");
+		    handler.setFormatter(new SimpleFormatter());
+
+		    // Add to the desired logger
+		    trace.addHandler(handler);
+		} catch (IOException e) {
+		}
 		
 		Kernel emu;
 		try {
 			emu = new Kernel(inputFile, outputFile);
 			emu.start();
-		} catch (IOException e) {
-			trace.log(Level.SEVERE, "IOException", e);
+		} catch (IOException ioe) {
+			trace.log(Level.SEVERE, "IOException", ioe);
+		} catch (Exception e){
+			trace.log(Level.SEVERE, "Exception", e);
 		}
 
 	}
 	
 	/**
-	 * 
+	 * Constructor
 	 * @param inputFile
 	 * @param outputFile
 	 * @throws IOException
@@ -99,6 +111,11 @@ public class Kernel {
 		} finally {
 			br.close();
 			wr.close();
+			//Dump memory
+			trace.info("\n"+mmu.toString());
+			//Dump memory
+			trace.info("\n"+cpu.toString());
+
 		}
 	}
 	
@@ -108,40 +125,164 @@ public class Kernel {
 	 */
 	public void masterMode() throws IOException {
 		trace.info("masterMode()-->");
+		trace.info("masterMode(): si="+cpu.getSi());
 		switch (cpu.getSi()) {
 		case READ:
 			mmu.writeBlock(cpu.getIrValue(), br.readLine());
+			cpu.setSi(CPU.Interupt.CONTINUE);
+			break;
 		case WRITE:
 			p.write(mmu.readBlock(cpu.getIrValue()));
+			cpu.setSi(CPU.Interupt.CONTINUE);
+			break;
 		case TERMINATE:
 			terminate();
+			break;
+		case CONTINUE:
+			//continue process execution
+			trace.info("masterMode(): continuing process "+p.id);
+			p.execute();
 		}
+		trace.info("masterMode()<--");
 	}
 	
 	/**
-	 * Starts new process.
+	 * Loads the program into memory and starts execution.
 	 * @throws IOException 
 	 */
 	public void load() throws IOException {
+		
 		trace.info("load()-->");
-		String jobData = br.readLine();
-		if (jobData != null && jobData.startsWith(Process.JOB_START)) {
-			trace.info("load(): Loading job "+jobData);
-			p = new Process(this, jobData, br, wr);
-			p.execute();
+		
+		String nextLine = br.readLine();
+
+		while (nextLine != null) {
+			//Check for EOJ
+			if (nextLine.startsWith(Process.JOB_END)
+					|| nextLine.startsWith(Process.JOB_END_ALT)) {
+				
+				writeProccess();
+				
+				trace.info("load(): Finished job "+p.id);
+				
+				//read next line
+				nextLine = br.readLine();
+				trace.info(nextLine);
+			}
+			
+			if (nextLine == null || nextLine.isEmpty()) {
+				trace.info("load(): skipping empty line...");
+				nextLine = br.readLine();
+				//exit();
+				continue;
+			}
+			else if (nextLine.startsWith(Process.JOB_START)) {
+				trace.info("load(): Loading job "+nextLine);
+				
+				//Clear memory
+				mmu.clear();
+				
+				//Parse Job Data
+				String id = nextLine.substring(4, 8);
+				int maxTime = Integer.parseInt(nextLine.substring(8, 12));
+				int maxPrints = Integer.parseInt(nextLine.substring(12, 16));
+				
+				//Reads first program line
+				String programLine = br.readLine();
+				int base = 0;
+				
+				//Write each block of program lines into memory
+				while (programLine != null) {
+					
+					if (programLine.equals(Process.JOB_END) 
+							|| programLine.equals(Process.JOB_END_ALT)
+							|| programLine.equals(Process.JOB_START)) {
+						trace.info("breaking on "+programLine);
+						break;
+					}
+					else if (programLine.equals(Process.DATA_START)) {
+						p = new Process(this, base, id, maxTime, maxPrints, br, wr);
+						p.startExecution();
+						break;
+					}
+
+					mmu.writeBlock(base, programLine);
+					base+=10;
+					programLine = br.readLine();
+				}
+
+			}
+			else {
+				trace.info("Unexpected line:"+nextLine);
+				trace.severe("load() Program error for "+p.id);
+				nextLine = br.readLine();
+			}
 		}
-		else {
-			trace.info("load(): No Job to load, exiting");
-		}
+
+		trace.info("load(): No more jobs, exiting");
+		exit();
+
+		trace.info("load()<--");
 	}
 	
 	/**
-	 * 
+	 * Called on program termination.
 	 * @throws IOException
 	 */
 	public void terminate() throws IOException {
 		trace.info("terminate()-->");
 		wr.write("\n\n");
 		load();
+		trace.info("terminate()<--");
+	}
+	
+	/**
+	 * Halts the OS
+	 */
+	public void exit() {
+		System.exit(0);
+	}
+	
+	/**
+	 * Returns the CPU
+	 * @return
+	 */
+	public CPU getCpu() {
+		return cpu;
+	}
+
+	/**
+	 * Returns the MMU
+	 * @return
+	 */
+	public MMU getMmu() {
+		return mmu;
+	}
+
+	public void slaveMode() {
+		cpu.fetch(mmu);
+		cpu.increment();
+		cpu.execute(mmu);		
+	}
+	
+	/**
+	 * Writes the process state and buffer to the output file
+	 * @throws IOException
+	 */
+	public void writeProccess() throws IOException {
+		trace.info("writeBuffer()-->");
+		wr.write(p.id+"    Normal Execution\n");
+		wr.write(cpu.getState());
+		//TODO write process time and lines printed: maybe something like: wr.write(p.getTime()+" "+p.getLines());
+		wr.newLine();
+		wr.newLine();
+		wr.newLine();
+		
+		ArrayList<String> buf = p.getOutputBuffer();
+		for (String line : buf) {
+			 wr.write(line);
+			 wr.newLine();
+		}
+		wr.flush();
 	}
 }
--- src/emu/os/Process.java
+++ src/emu/os/Process.java
@@ -4,8 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import emu.hw.CPU;
 
 /**
  * 
@@ -16,11 +17,12 @@ public class Process {
 	/**
 	 * For tracing
 	 */
-	static Logger trace = Logger.getLogger("emu.os");
+	static Logger trace = Logger.getLogger("emuos");
 	
 	public static final String JOB_START = "$AMJ";
 	public static final String DATA_START = "$DTA";
 	public static final String JOB_END = "$EOJ";
+	public static final String JOB_END_ALT = "$END";
 	
 	/**
 	 * Buffers the program output
@@ -30,29 +32,41 @@ public class Process {
 	 * The kernel
 	 */
 	Kernel kernel;
+	/**
+	 * The address where data can be loaded.
+	 */
+	int baseDataAddr;
+	/**
+	 * Process ID 
+	 */
+	String id;
+	/**
+	 * Max number of time units of execution
+	 */
+	int maxTime;
+	/**
+	 * Max number of prints
+	 */
+	int maxPrints;
+	
 	//TODO ProcessControlBlock
 	
 	/**
 	 * 
 	 * @param kernel Reference to the kernel instance 
+	 * @param maxPrints2 
 	 * @param jobData The job id
 	 * @param program The input stream from which we obtain the program lines
 	 * @param output The output stream we write to.
 	 */
-	public Process(Kernel kernel, String jobData, BufferedReader program, BufferedWriter output) {
-		trace.info("new Process for "+jobData);
+	public Process(Kernel kernel, int baseDataAddr, String id, int maxTime, int maxPrints, BufferedReader program, BufferedWriter output) {
+		trace.info("id="+id+", maxTime="+maxTime+", maxPrints="+maxPrints+", baseDataAddr="+baseDataAddr);
 		outputBuffer = new ArrayList<String>();
-		try {
-			this.kernel = kernel;
-			String jobLine = program.readLine();
-			
-			jobLine.toCharArray();
-			//parse job line
-			
-			//loop until $DTA, load instructions into MMU
-		} catch (IOException e) {
-			trace.log(Level.SEVERE, "", e);
-		}
+		this.kernel = kernel;
+		this.baseDataAddr = baseDataAddr;
+		this.id = id;
+		this.maxTime = maxTime;
+		this.maxPrints = maxPrints;
 	}
 	
 	/**
@@ -60,7 +74,9 @@ public class Process {
 	 * @throws IOException 
 	 */
 	public void startExecution() throws IOException {
+		trace.info("startExecution()-->");
 		kernel.getCpu().setIc(0);
+		kernel.getCpu().setSi(CPU.Interupt.TERMINATE);
 		execute();
 	}
 	
@@ -69,8 +85,13 @@ public class Process {
 	 * @throws IOException 
 	 */
 	public void execute() throws IOException {
+		trace.info("execute()-->");
+		while(true){
 		//testing how we could return to master mode during execution
-		kernel.masterMode();
+			kernel.slaveMode();
+			kernel.masterMode();
+		}
+		
 	}
 	
 	/**
@@ -79,6 +100,14 @@ public class Process {
 	 */
 	public void write(String data) {
 		outputBuffer.add(data);
+	}
+	
+	/**
+	 * return output buffer to caller
+	 * @return
+	 */
+	public ArrayList<String> getOutputBuffer() {
+		return outputBuffer;
 	}
 	
 	/**
